d7306f1204eef2cec1a0f3dae9dd3e6636464c2c
Meritxell <meritxellara@gmail.com>
2025-06-10 11:32:32 +0200

    Initialize

---
+++ src/main/java/cat/itacademy/s04/t01/n02/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package cat.itacademy.s04.t01.n02.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class HelloWorldController {
+
+    @GetMapping("/HelloWorld")
+    public String salute(@RequestParam(name = "name", defaultValue = "UNKNOWN") String name) {
+        return "Hello, " + name + ", you are running a Gradle project.";
+    }
+
+    @GetMapping("/HelloWorld2/{name}")
+    public String salute2(@PathVariable(name = "name", required = false) String name) {
+        if (name == null) {
+            name = "UNKNOWN";
+        }
+        return "Hello, " + name + ", you are running a Gradle project.";
+    }
+}
